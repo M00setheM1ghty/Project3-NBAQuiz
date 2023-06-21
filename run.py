b224c86fbf609d29963cf06a5a46d06539ab9a9e
Thomas Baumann <tbaumann115@gmail.com>
2023-06-21 09:31:41 +0000

    create game and start screen function

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
+import random
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -15,6 +16,39 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('NBAFinalsData')
 
-info = SHEET.worksheet('NBAFinals')
+wsheet = SHEET.worksheet('NBAFinals')
 
-data = info.get_all_values()
+def play_game():
+    """
+    calls the necessary functions to go through the game
+    """
+    display_start_screen()
+
+
+def display_start_screen():
+    """
+    explain the game and start quiz when player is ready
+    """
+    print('Welcome!\nThis quiz will be testing your knowledge on the NBA basketball finals.')
+    data_str = input('Are you ready? Y/N:\n')
+    
+    if data_str == 'Y' or data_str == 'y':
+        print('Let us start quizzing!')
+    else:
+        print('Back to the homescreen!')
+
+def set_difficulty():
+    """
+    set game difficulty
+    """
+
+def set_length():
+    """
+    set game length
+    """
+def create_question():
+    """
+    create question from the NBA finals spreadsheet
+    """    
+
+play_game()
