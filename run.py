6c9004514eeb2d475f0c0152aeb68b246a07acb1
Thomas Baumann <tbaumann115@gmail.com>
2023-06-21 12:51:10 +0000

    modify functions to return values

--- run.py
+++ run.py
@@ -25,6 +25,7 @@ def play_game():
     """
     display_start_screen()
     set_difficulty()
+    set_question_amount()
     create_questions()
 
 
@@ -35,48 +36,54 @@ def display_start_screen():
     print('Welcome!\nThis quiz will be testing your knowledge on the NBA basketball finals.')
     data_str = input('Are you ready? Y/N:\n')
 
-    if data_str == 'Y' or data_str == 'y':
+    if data_str.lower() == 'y':
         print('Let us start quizzing!')
     else:
         print('Back to the homescreen!')
+        display_start_screen()
 
 
 def set_difficulty():
     """
     set game difficulty
     """
-    difficulty_str = input('How hard do you want this to be? ROOKIE/AMATEUR/PRO')
-    if difficulty_str == 'ROOKIE':
-        pass
-    elif difficulty_str == 'AMATEUR':
-        pass
-    elif difficulty_str == 'PRO':
-        pass
-    else: 
+    difficulty_str = input('Which difficulty level?\nRookie/Amateur/Pro:\n')
+    difficulty = ''
+    if difficulty_str.lower() == 'rookie':
+        difficulty += 'rookie'
+    elif difficulty_str.lower() == 'amateur':
+        difficulty += 'amateur'
+    elif difficulty_str.lower() == 'pro':
+        difficulty += 'pro'
+    else:
         print('Your input is not valid. Try again')
         set_difficulty()
+    return difficulty
     
-def set_length():
+
+
+def set_question_amount():
     """
     set amount of questions to be asked
     """
-    length_str = input('How many questions would you like to answer? 5/10/15')
+    length_str = input('How many questions would you like to answer? 5/10/15\n')
+    question_amount = 0
     if int(length_str) == 5:
-        pass
+        question_amount += 5
     elif int(length_str) == 10:
-        pass
+        question_amount += 10
     elif int(length_str) == 15:
-        pass
+        question_amount += 15
     else:
-        print('Your input is not valid. Try again')
-        
-
+        print('Your input is not valid. Try again')    
+    return question_amount
+    
 
 def create_questions():
     """
     create question from the NBA finals spreadsheet
     """
-    #question_index = random.randint(1, 69)
+    #question_index = random.randint(2, 64)
 
 
 play_game()
