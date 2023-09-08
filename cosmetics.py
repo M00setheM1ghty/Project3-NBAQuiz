41e55fae7af2fb607ca1752d1564883e9411c473
Thomas Baumann <tbaumann115@gmail.com>
2023-09-08 13:45:14 +0000

    added cosmetics.py to declutter run.py

---
+++ cosmetics.py
@@ -0,0 +1,115 @@
+from termcolor import cprint
+
+
+# termcolor functions
+def print_red(x):
+    """
+    termcolor functions
+    """
+    cprint(x, 'red')
+
+
+def print_green(x):
+    """
+    termcolor functions
+    """
+    cprint(x, 'green')
+
+
+def print_blue(x):
+    """
+    termcolor functions
+    """
+    cprint(x, 'blue')
+
+
+# ascii art functions
+def art_1():
+    """
+    print ascii art
+    """
+    print("""\ 
+        o
+/|   o         o
+\|=--            o
+   ##
+                   \\
+                /   \O
+               O_/   T
+               T    /|
+               |\  | |
+_______________|_|________
+""")
+
+
+def art_2():
+     """
+    print ascii art
+    """
+     print("""\ 
+    o      |   __   |
+      \_ O |  |__|  |
+   ____/ \ |___WW___|
+   __/   /     ||
+               ||
+               ||
+_______________||________________
+""")
+
+
+def art_3():
+     """
+    print ascii art
+    """
+     print("""\ 
+|__  o\\
+| W    \O
+|       |\_
+|      /-\\
+|    /     \\
+|
+|
+""")
+
+
+def art_4():
+     """
+    print ascii art
+    """
+     print("""\ 
+o- - -  \o          __|
+   o/   /|          vv`\\
+  /|     |              |
+   |    / \_            |
+  / \   |               |
+ /  |                   |
+""")
+
+
+def art_5():
+     """
+    print ascii art
+    """
+     print("""\ 
+          <\ __
+            /\>   \O___
+          /       __\   
+        /       /  /
+       |          /
+_______|_______________
+""")
+
+
+def art_6():
+     """
+    print ascii art
+    """
+     print("""\ 
+  |__  o\\
+| W    \O
+|       |\_         |\\
+|      /-\           \O
+|    /     \          |
+|                    /|
+|                   |  \\
+""")
